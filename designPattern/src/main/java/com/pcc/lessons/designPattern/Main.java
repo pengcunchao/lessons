aca5c4f9c2cc525eface3e8bb57cc79636dda2bb
manson <v-mpeng@expedia.com>
2019-10-12 17:26:47 +0800

    visitor,composite pattern

--- designPattern/src/main/java/com/pcc/lessons/designPattern/Main.java
+++ designPattern/src/main/java/com/pcc/lessons/designPattern/Main.java
@@ -1,12 +1,19 @@
 package com.pcc.lessons.designPattern;
 
+import com.pcc.lessons.designPattern.composite.Directory;
+import com.pcc.lessons.designPattern.composite.Entry;
+import com.pcc.lessons.designPattern.composite.File;
 import com.pcc.lessons.designPattern.iterator.ArrayList;
 import com.pcc.lessons.designPattern.iterator.Book;
 import com.pcc.lessons.designPattern.iterator.Collection;
 import com.pcc.lessons.designPattern.iterator.Iterator;
+import com.pcc.lessons.designPattern.visitor.Reader;
+import com.pcc.lessons.designPattern.visitor.Visitor;
 
 /**
  * 1. Iterator：提供一种方法顺序的访问一个聚合对象中各个元素，而又不暴露该对象的内部表示；将对集合的遍历与集合的具体实现分类。
+ * 2. Composite: 使内容和容器具有一致性，统一内容和容器的处理。一般来说，树结构都可以使用Composite模式
+ * 3. Visitor模式：数据结构与数据的处理分离
  * **/
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +30,18 @@ public class Main {
             System.out.println(temp);
         }
         System.out.println("design pattern");
+
+        //
+        Entry directory = new Directory("usr",0);
+        directory.add(new File("root",10));
+        directory.add(new File("pcc",20));
+
+        Entry admin = new Directory("admin",0);
+        admin.add(new File("admin1",5));
+        directory.add(admin);
+
+        Visitor visitor = new Reader();
+
+       directory.accept(visitor);
     }
 }
